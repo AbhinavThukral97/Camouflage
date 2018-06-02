0d5aeb4507a511b37fdf5db8890974a89eebd983
Abhinav Thukral <thukral.abhinav@gmail.com>
2018-06-02 13:48:28 +0530

    MONGODB_URI Setup

--- models/mongoose.js
+++ models/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Camouflage');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
